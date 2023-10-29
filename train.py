4d63ed06181c301f37fa3cdf63074b1841022949
Alex <139784963+SliverCreator@users.noreply.github.com>
2023-10-29 17:15:49 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,117 @@
+# -*- coding: UTF-8 -*-
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import datasets
+import models
+import os, shutil
+import argparse
+import test
+import torchvision
+import settings
+from PIL import Image
+from torch.nn.parallel import DataParallel
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+# GPU / cpu
+IS_USE_GPU = 1
+# 将num_workers设置为等于计算机上的CPU数量
+worker_num = 8
+
+if IS_USE_GPU:
+    import torch_util  # 导入PyTorch的神经网络模块
+
+    # 通过os.environ["CUDA_VISIBLE_DEVICES"]指定所要使用的显卡，如：
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"  # 设置可见的GPU设备，这里设置为0和1
+    device = torch_util.select_device()  # 选择设备，这里使用CPU
+else:
+    device = torch.device("cpu")
+# Hyper Parameters
+num_epochs = 70
+batch_size = 512
+learning_rate = 0.001
+
+
+def clean_unvalid_png():
+    print("clean_unvalid_png")
+    for dir in [settings.TRAIN_DATASET_PATH, settings.TEST_DATASET_PATH, settings.PREDICT_DATASET_PATH]:
+        ok = 0
+        fail = 0
+        dir = os.path.abspath(dir)
+        for file in os.listdir(dir):
+            file = os.path.join(dir, file)
+            try:
+                image = Image.open(file)
+                ok += 1
+            except Exception as e:
+                fail += 1
+                # print(e)
+                os.remove(file)
+        print(f"{dir}   [+{ok} / -{fail}]")
+
+
+str_name = "cnn"
+base_prefix = f"./weights/{str_name}"
+
+
+def main(args):
+    # RES18/CNN
+    cnn = models.RES18()
+    cnn = cnn.to(device)
+    cnn = DataParallel(cnn)
+
+    cnn.train()
+    criterion = nn.MultiLabelSoftMarginLoss()
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+
+    if args.resume:
+        cnn.load_state_dict(torch.load(args.model_path, map_location=device))
+
+    max_acc = 0
+    # Train the Model
+    train_dataloader = datasets.get_train_data_loader()
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_dataloader):  # 遍历每一批训练数据
+            if IS_USE_GPU:
+                images = Variable(images).to(device)
+                labels = Variable(labels.float()).to(device)
+            else:
+                images = Variable(images)
+                labels = Variable(labels.float())
+            predict_labels = cnn(images)  # 通过CNN模型进行预测
+            loss = criterion(predict_labels, labels)  # 计算损失值
+            optimizer.zero_grad()  # 清空梯度
+            loss.backward()  # 反向传播计算梯度
+            optimizer.step()  # 根据梯度更新模型参数
+            epoch_name = "%s_%03g.pt" % (base_prefix, epoch)  # 保存当前模型权重
+            if (i + 1) % 2 == 0:  # 每2个批次打印一次训练信息
+                print("epoch: %03g \t step: %03g \t loss: %.5f \t\r" % (epoch, i + 1, loss.item()))  # 保存当前模型权重
+                torch.save(cnn.state_dict(), epoch_name)  # 保存当前模型权重
+        print("epoch: %03g \t step: %03g \t loss: %.5f \t" % (epoch, i, loss.item()))  # 保存当前模型权重
+        torch.save(cnn.state_dict(), epoch_name)  # 保存当前模型权重
+        print(epoch_name)  # 保存当前模型权重
+        acc = test.test_data(epoch_name)  # 测试当前模型在测试集上的准确率
+        # 在循环语句中将epoch和acc分别输出到txt文件中的第一列和第二列
+        with open("./results-acc.txt", "a") as f:
+            f.write(f"{epoch},{acc}\n")
+        torch_util.plot_result()
+        if max_acc < acc:
+            print("update accuracy %.5f." % acc)
+            max_acc = acc
+            shutil.copy(epoch_name, f"{base_prefix}_best.pt")
+        else:
+            print("do not update %.5f." % acc)
+
+    torch.save(cnn.state_dict(), f"{base_prefix}_last.pt")
+    print("save last model")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="load path")
+    parser.add_argument('--model-path', type=str, default=f"{base_prefix}_0.pt")
+    parser.add_argument('--resume', action='store_true')
+
+    args = parser.parse_args()
+    # don't clean
+    # clean_unvalid_png()
+    main(args)
